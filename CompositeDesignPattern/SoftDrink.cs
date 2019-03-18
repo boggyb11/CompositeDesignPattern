3d32f706e03a8dc2f37616a867d7c80640824657
Benjamin Fitzpatrick <Benjamin_Fitzpatrick@allfleetservices.co.uk>
2019-03-18 15:20:44 +0000

    12

---
+++ CompositeDesignPattern/SoftDrink.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CompositeDesignPattern
+{
+    public abstract class SoftDrink
+    {
+        public int Calories { get; set; }
+
+        public List<SoftDrink> Flavors { get; set; }
+
+        public SoftDrink(int calories)
+        {
+            Calories = calories;
+            Flavors = new List<SoftDrink>();
+        }
+
+        public void DisplayCalories()
+        {
+            Console.WriteLine(this.GetType().Name + ":" + this.Calories.ToString() + " calories.");
+            foreach (var drink in this.Flavors)
+            {
+                drink.DisplayCalories();
+            }
+        }
+    }
+
+}
---
+++ CompositeDesignPattern/VanillaCola.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CompositeDesignPattern
+{
+    // Leaf classes
+    public class VanillaCola : SoftDrink
+    {
+        public VanillaCola(int calories) : base(calories) { }
+    }
+
+    public class CherryCola : SoftDrink
+    {
+        public CherryCola(int calories) : base(calories) { }
+    }
+
+    public class StrawberryRootBeer : SoftDrink
+    {
+        public StrawberryRootBeer(int calories) : base(calories) { }
+    }
+
+    public class VanillaRootBeer : SoftDrink
+    {
+        public VanillaRootBeer(int calories) : base(calories) { }
+    }
+
+    public class LemonLime : SoftDrink
+    {
+        public LemonLime(int calories) : base(calories) { }
+    }
+
+    //Composite classes
+    public class Cola : SoftDrink
+    {
+        public Cola(int calories) : base(calories) { }
+    }
+
+    public class RootBeer : SoftDrink
+    {
+        public RootBeer(int calories) : base(calories) { }
+    }
+
+    // Composite class, root node
+    public class SodaWater : SoftDrink
+    {
+        public SodaWater(int calories) : base(calories) { }
+    }
+}
